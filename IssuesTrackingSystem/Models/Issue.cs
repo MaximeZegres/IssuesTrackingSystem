58a61b35c55a4d0e89770b79144d99a032bfd776
maxime.zegres <maxime.zegres@gmail.com>
2019-05-16 23:47:44 +0200

    Add Model to the project

---
+++ IssuesTrackingSystem/Models/Issue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IssuesTrackingSystem.Models
+{
+    public class Issue
+    {
+        public Guid Id { get; set; }
+        public string UserPC { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public enum Priority { Low, Medium, High }
+        public bool IsComplete { get; set; }
+    }
+}
