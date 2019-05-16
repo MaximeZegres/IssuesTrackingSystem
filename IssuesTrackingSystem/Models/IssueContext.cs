20bde904f2a576ddf1c4a84f05f62f757db370df
maxime.zegres <maxime.zegres@gmail.com>
2019-05-16 23:55:30 +0200

    Add DbContext

---
+++ IssuesTrackingSystem/Models/IssueContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IssuesTrackingSystem.Models
+{
+    public class IssueContext : DbContext
+    {
+        public IssueContext(DbContextOptions<IssueContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
