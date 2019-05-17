b6415350ddab057249840e1561f6356e5cf88450
maxime.zegres <maxime.zegres@gmail.com>
2019-05-17 10:33:39 +0200

    Add connectionString and DbContext to Startup.cs

--- IssuesTrackingSystem/Startup.cs
+++ IssuesTrackingSystem/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IssuesTrackingSystem.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +27,9 @@ namespace IssuesTrackingSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<IssueContext>
+                (options => options.UseSqlServer(Configuration.GetConnectionString("IssueContextConnectionString")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
