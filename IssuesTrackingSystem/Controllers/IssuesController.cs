749ac0a8d5acee46370ad1f7694c1fbbc937e154
maxime.zegres <maxime.zegres@gmail.com>
2019-05-19 16:15:42 +0200

    Configure get methods

---
+++ IssuesTrackingSystem/Controllers/IssuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IssuesTrackingSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IssuesTrackingSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssuesController : ControllerBase
+    {
+        private readonly IssueContext _context;
+
+        public IssuesController(IssueContext context)
+        {
+            _context = context;       
+        }
+
+
+
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Issue>>> GetIssues()
+        {
+            return await _context.Issues.ToListAsync();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Issue>> GetIssueItem(long id)
+        {
+            var issueItem = await _context.Issues.FindAsync(id);
+
+            if (issueItem == null)
+            {
+                return NotFound();
+            }
+
+            return issueItem;
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
